a7b899037e08eaab704a06cf4771eaeab0c7ae3d
rafaelarrieta <rarrieta@minsait.com>
2023-06-26 13:22:32 +0200

    init

--- dist/index.js
+++ dist/index.js
@@ -7,13 +7,24 @@ class NewEmpleyee {
         this.contractDate = new Date();
     }
     calculateSalary() {
-        return 0;
+        return this.salary * 12;
     }
     getSeniority() {
-        return 0;
+        const today = new Date();
+        let age = today.getFullYear() - this.contractDate.getFullYear();
+        const month = today.getMonth() - this.contractDate.getMonth();
+        if (month < 0 || (month === 0 && today.getDate() < this.contractDate.getDate())) {
+            age--;
+        }
+        return age;
     }
     increaseSalary(percentage) {
+        this.salary = (this.salary * (100 + percentage)) / 100;
     }
 }
-console.log(new NewEmpleyee('pepe', 'programador', 1500));
+const empleado1 = new NewEmpleyee('pepe', 'programador', 1500);
+console.log(empleado1.calculateSalary());
+console.log(empleado1.getSeniority());
+empleado1.increaseSalary(15);
+console.log(empleado1.calculateSalary());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -27,18 +27,29 @@ class NewEmpleyee implements Employee {
     }
 
     calculateSalary(): number {
-        return 0;
+        return this.salary * 12;
     }
 
 
     getSeniority(): number {
-        return 0;
+        const today = new Date();
+        let age = today.getFullYear() - this.contractDate.getFullYear();
+        const month = today.getMonth() - this.contractDate.getMonth();
+
+        if (month < 0 || (month === 0 && today.getDate() < this.contractDate.getDate())) {
+            age--;
+        }
+
+        return age;
     }
 
     increaseSalary(percentage: number): void {
+        this.salary = (this.salary * (100 + percentage)) / 100
     }
-
-
 }
+const empleado1  =  new NewEmpleyee('pepe', 'programador', 1500)
 
-console.log(new NewEmpleyee('pepe', 'programador', 1500))
+console.log(empleado1.calculateSalary())
+console.log(empleado1.getSeniority())
+empleado1.increaseSalary(15)
+console.log(empleado1.calculateSalary())
